daa6d2cb34f35bd15d6029614088a0f5a5f40ed0
leenaAhmed <leena3792@gmail.com>
2021-10-30 19:29:39 +0200

    add

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const btnsNext = document.querySelectorAll(".btn-next");
+const btnsPrev = document.querySelectorAll(".btn-prev");
+const Prograss = document.getElementById("prograss");
+const formStep = document.querySelectorAll(".formStep");
+const prograssStep = document.querySelectorAll(".prograss-step");
+
+let formStepNumber = 0;
+
+btnsNext.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepNumber++;
+    updatePrograss();
+    updatePrograssPar();
+  });
+});
+btnsPrev.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepNumber--;
+    updatePrograss();
+    updatePrograssPar();
+  });
+});
+
+function updatePrograss() {
+  formStep.forEach((formstep) => {
+    formstep.classList.contains("form-step-active") &&
+      formstep.classList.remove("form-step-active");
+  });
+  formStep[formStepNumber].classList.add("form-step-active");
+  console.log(formStepNumber);
+}
+
+function updatePrograssPar() {
+  prograssStep.forEach((prostep, index) => {
+    if (index < formStepNumber + 1) {
+      prostep.classList.add("prograss-step-active");
+    } else {
+      prostep.classList.remove("prograss-step-active");
+    }
+  });
+}
